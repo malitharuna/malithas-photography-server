723cb0fd2faad8b04dd8f1056bb6defc98af71fd
malitharuna <msmalitharuna@gmail.com>
2023-01-13 23:55:20 +0600

    db collection add

--- index.js
+++ index.js
@@ -14,6 +14,16 @@ app.use(express.json());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.0b62e8i.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+async function run(){
+    try {
+        const serviceCollection =client.db('malithas-photography').collection('serviceCollection')
+    }
+    finally{
+
+    }
+}
+run().catch(err=> console.error(err))
+
 
 app.get('/',(req,res)=>{
     res.send("Server is Running");
