d95501a70dc3818c929b47c4debf4646c032d0df
malitharuna <msmalitharuna@gmail.com>
2023-01-14 15:42:20 +0600

    service

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
 
@@ -17,6 +17,47 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 async function run(){
     try {
         const serviceCollection =client.db('malithas-photography').collection('serviceCollection')
+        const feedbackCollection = client.db('malithas-photography').collection('feedbackCollection');
+
+        // get all services
+        app.get('/services', async(req, res)  =>{
+            const limitQuery=parseInt(req.query.limit);
+            const sort = {date:-1}
+            if(limitQuery)
+            {
+                const cursor = serviceCollection.find({}).sort(sort).limit(limitQuery);
+                const result = await cursor.toArray();
+                return res.send(result)
+            }else{
+                const cursor = serviceCollection.find({}).sort(sort);
+                const result = await cursor.toArray();
+                 return res.send(result);
+            }
+        })
+
+        // service description by id
+        app.get('/services/:id', async(req, res) =>{
+            const id =req.params.id;
+            const query = {_id: ObjectId(id)};
+            const service = await serviceCollection.findOne(query);
+            res.send(service);
+        })
+
+         // store review/feedback
+
+         app.post('/add/feedback',async(req,res)=>{
+            const userFeedback = req.body;
+
+            userFeedback.date = new Date();
+
+            const result = await feedbackCollection.insertOne(userFeedback);
+
+            if(result.acknowledged)
+            {
+                res.send({insert:true})
+            }
+       })
+
     }
     finally{
 
